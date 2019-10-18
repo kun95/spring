68f6d4cf5d5280f0dda3185de132d87254aac2c5
guohuaikun <guohuaikun@souche.com>
2019-10-18 23:03:54 +0800

    优化

--- push.sh
+++ push.sh
@@ -3,9 +3,16 @@ git status
 git add .
 echo "-------2. 添加到暂存区完毕-------"
 read -p "提交信息:" commitinfo
-git commit -m $commitinfo
+if  [ ! -n "$commitinfo" ] ;then
+    git commit -m "优化"
+else
+    git commit -m $commitinfo
+fi
 echo "-------3. 添加到版本库完毕-------"
 git push -u origin master
 echo "-------4. 推送到github完毕-------"
 git push -u gitee master
 echo "-------5. 推送到gitee完毕-------"
+echo "https://kun95.github.io/spring/"
+echo "https://kun95.gitee.io/spring/"
+echo "https://gitee.com/kun95/spring/pages"
